4d7d450ea64d1b7e057582cb0c91f9c26eb474b9
Dmitriy Rybakov <dmitriy.rybakov@lamoda.ru>
2017-02-07 19:02:04 +0300

    Refactoring. Splitted into separate modules

---
+++ exporter/exporter.go
@@ -0,0 +1,201 @@
+package exporter
+
+import (
+	"time"
+	"sync"
+	"fmt"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/log"
+
+	"camino.ru/newrelic_exporter/newrelic"
+)
+
+// Namespace for metrics
+const NameSpace = "newrelic"
+
+type Metric struct {
+	App   string
+	Name  string
+	Value float64
+	Label string
+}
+
+type Exporter struct {
+	mu              sync.Mutex
+	duration, error prometheus.Gauge
+	totalScrapes    prometheus.Counter
+	metrics         map[string]prometheus.GaugeVec
+	api             *newrelic.NewRelicAPI
+}
+
+func NewExporter(api *newrelic.NewRelicAPI) *Exporter {
+	return &Exporter{
+		duration: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: NameSpace,
+			Name:      "exporter_last_scrape_duration_seconds",
+			Help:      "The last scrape duration.",
+		}),
+		totalScrapes: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: NameSpace,
+			Name:      "exporter_scrapes_total",
+			Help:      "Total scraped metrics",
+		}),
+		error: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: NameSpace,
+			Name:      "exporter_last_scrape_error",
+			Help:      "The last scrape error status.",
+		}),
+		metrics: map[string]prometheus.GaugeVec{},
+		api: api,
+	}
+}
+
+func (e *Exporter) scrape(ch chan <- Metric) {
+	e.error.Set(0)
+	e.totalScrapes.Inc()
+
+	startTime := time.Now()
+	log.Infof("Starting new scrape at %v.", startTime)
+
+	var apps newrelic.AppList
+	err := apps.Get(e.api)
+	if err != nil {
+		log.Error(err)
+		e.error.Set(1)
+	}
+
+	for _, app := range apps.Applications {
+		for name, value := range app.AppSummary {
+			ch <- Metric{
+				App:   app.Name,
+				Name:  name,
+				Value: value,
+				Label: "application_summary",
+			}
+		}
+
+		for name, value := range app.UsrSummary {
+			ch <- Metric{
+				App:   app.Name,
+				Name:  name,
+				Value: value,
+				Label: "end_user_summary",
+			}
+		}
+	}
+
+	var wg sync.WaitGroup
+
+	for i := range apps.Applications {
+		app := apps.Applications[i]
+
+		wg.Add(1)
+		api := e.api
+
+		go func() {
+			defer wg.Done()
+
+			// Getting metric names
+			names := make(newrelic.MetricNames)
+
+			err = names.Get(api, app.ID)
+			log.Infof("Scraped %v metrics for app %v", len(names), app.ID)
+			if err != nil {
+				log.Error(err)
+				e.error.Set(1)
+			}
+
+			// Getting metric data
+			var data newrelic.MetricData
+
+			err = data.Get(api, app.ID, names)
+			log.Infof("Scraped %v metric datas for app %v", len(data.Metric_Data.Metrics), app.ID)
+			if err != nil {
+				log.Error(err)
+				e.error.Set(1)
+			}
+
+			// Sending metrics
+			for _, set := range data.Metric_Data.Metrics {
+				if len(set.Timeslices) == 0 {
+					continue
+				}
+
+				// As we set summarise=true there will only be one timeseries.
+				for name, value := range set.Timeslices[0].Values {
+					if v, ok := value.(float64); ok {
+						ch <- Metric{
+							App:   app.Name,
+							Name:  name,
+							Value: v,
+							Label: set.Name,
+						}
+					}
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	close(ch)
+	e.duration.Set(float64(time.Now().UnixNano()- startTime.UnixNano()) / 1000000000)
+}
+
+func (e *Exporter) recieve(ch <-chan Metric) {
+	for metric := range ch {
+		id := fmt.Sprintf("%s_%s", NameSpace, metric.Name)
+
+		if m, ok := e.metrics[id]; ok {
+			m.WithLabelValues(metric.App, metric.Label).Set(metric.Value)
+		} else {
+			g := prometheus.NewGaugeVec(
+				prometheus.GaugeOpts{
+					Namespace: NameSpace,
+					Name:      metric.Name,
+				},
+				[]string{"app", "component"})
+
+			e.metrics[id] = *g
+		}
+	}
+}
+
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
+	for _, m := range e.metrics {
+		m.Describe(ch)
+	}
+
+	ch <- e.duration.Desc()
+	ch <- e.totalScrapes.Desc()
+	ch <- e.error.Desc()
+}
+
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
+	// Align requests to minute boundary.
+	// As time.Round rounds to the nearest integar rather than floor or ceil,
+	// subtract 30 seconds from the time before rounding.
+	e.api.To = time.Now().Add(-time.Second * 30).Round(time.Minute).UTC()
+	e.api.From = e.api.To.Add(-time.Duration(e.api.Period) * time.Second)
+
+	metricChan := make(chan Metric)
+
+	go e.scrape(metricChan)
+
+	e.recieve(metricChan)
+
+	ch <- e.duration
+	ch <- e.totalScrapes
+	ch <- e.error
+
+	for _, m := range e.metrics {
+		m.Collect(ch)
+	}
+}
---
+++ newrelic/newrelic.go
@@ -0,0 +1,378 @@
+package newrelic
+
+import (
+	"fmt"
+	"encoding/json"
+	"bytes"
+	"io"
+	"strconv"
+	"net/url"
+	"time"
+	"net/http"
+	"crypto/tls"
+	"io/ioutil"
+
+	"github.com/prometheus/log"
+	"github.com/tomnomnom/linkheader"
+
+	"camino.ru/newrelic_exporter/config"
+)
+
+// Module version
+const Version = "0.1b"
+
+// User-Agent string
+const UserAgent = "Prometheus-NewRelic-Exporter/" + Version
+
+// Chunk size of metric requests
+const ChunkSize = 10
+
+var cfg config.Config
+
+type NewRelicAPI struct {
+	server          url.URL
+	apiKey          string
+	service		string
+	From            time.Time // remove
+	To              time.Time // remove
+	Period          int // remove
+	unreportingApps bool
+	client          *http.Client
+}
+
+func NewNewRelicAPI(cfg config.Config) *NewRelicAPI {
+	serverURL, err := url.Parse(cfg.NRApiServer)
+	if err != nil {
+		log.Fatal("Could not parse API URL: ", err)
+	}
+	if cfg.NRApiKey == "" {
+		log.Fatal("Cannot continue without an API key.")
+	}
+	if cfg.NRService == "" {
+		log.Fatal("Cannot continue without NewRelic service selected")
+	}
+
+	client := &http.Client{Timeout: cfg.NRTimeout}
+
+	debugMode := true
+	if debugMode {
+		proxyUrl, _ := url.Parse("https://localhost:8888")
+		transport := &http.Transport{}
+		transport.Proxy = http.ProxyURL(proxyUrl)
+		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		client.Transport = transport
+	}
+
+	return &NewRelicAPI{
+		server: *serverURL,
+		apiKey: cfg.NRApiKey,
+		service: cfg.NRService,
+		client: client,
+		Period: cfg.NRPeriod,
+	}
+}
+
+func (a *NewRelicAPI) req(path string, params string) ([]byte, error) {
+
+	u, err := url.Parse(a.server.String() + path)
+	if err != nil {
+		return nil, err
+	}
+	u.RawQuery = params
+
+	log.Debug("Making API call: ", u.String())
+
+	req := &http.Request{
+		Method: "GET",
+		URL:    u,
+		Header: http.Header{
+			"User-Agent": {UserAgent},
+			"X-Api-Key":  {a.apiKey},
+		},
+	}
+
+	var data []byte
+
+	return a.httpget(req, data)
+}
+
+func (a *NewRelicAPI) httpget(req *http.Request, in []byte) (out []byte, err error) {
+	resp, err := a.client.Do(req)
+	if err != nil {
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	resp.Body.Close()
+	out = append(in, body...)
+
+	// Read the link header to see if we need to read more pages.
+	links := linkheader.Parse(resp.Header.Get("Link"))
+
+	relLast := links.FilterByRel("last")
+	if len(relLast) > 0 {
+		u, err := url.Parse(relLast[0].URL)
+		if err != nil {
+			log.Errorf("Error parsing 'last' relation link. %v", err)
+		}
+		log.Debugf("Found %v pages for %s", u.Query().Get("page"), req.URL)
+	}
+
+	relNext := links.FilterByRel("next")
+	if len(relNext) > 0 {
+		u := new(url.URL)
+
+		u, err = url.Parse(relNext[0].URL)
+		if err != nil {
+			return
+		}
+		cursor := u.Query().Get("cursor")
+
+		if cursor != "" {
+			query := req.URL.Query()
+
+			query.Set("cursor", cursor)
+			query.Del("page")
+
+			req.URL.RawQuery = query.Encode()
+
+			return a.httpget(req, out)
+		}
+	}
+
+	return
+}
+
+type AppList struct {
+	Applications []Application
+}
+
+type Application struct {
+	ID         int                `yaml:"id"`
+	Name       string             `yaml:"name"`
+	Health     string             `json:"health_status"`
+	AppSummary map[string]float64 `json:"application_summary"`
+	UsrSummary map[string]float64 `json:"end_user_summary"`
+}
+
+func (a *AppList) Get(api *NewRelicAPI) error {
+	if len(cfg.NRApps) > 0 {
+		// Using local app list instead of getting it from API - one API call less
+		log.Infof("Getting application list from config: %v", cfg.NRApps)
+		for _, app := range cfg.NRApps {
+			a.Applications = append(a.Applications, Application{ID: app.Id, Name: app.Name})
+		}
+		return nil
+	}
+
+	log.Infof("Requesting application list from %s.", api.server.String())
+	body, err := api.req(fmt.Sprintf("/v2/%s.json", api.service), "")
+	if err != nil {
+		log.Error("Error getting application list: ", err)
+		return err
+	}
+
+	dec := json.NewDecoder(bytes.NewReader(body))
+	for {
+		page := new(AppList)
+		if err := dec.Decode(page); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Error("Error decoding application list: ", err)
+			return err
+		}
+
+		a.Applications = append(a.Applications, page.Applications...)
+	}
+
+	return nil
+}
+
+type MetricNames map[string][]string
+
+func (m *MetricNames) Get(api *NewRelicAPI, appID int) error {
+	log.Infof("Requesting metrics names for application id %d.", appID)
+	path := fmt.Sprintf("/v2/%s/%s/metrics.json", api.service, strconv.Itoa(appID))
+
+	// We will only make filtered requests for metric names. Otherwise there are too many of them (tens of thousands)
+	var i int
+	var filter string
+	channel := make(chan MetricNames)
+
+	for i, filter = range cfg.NRMetricFilters {
+		go func(filter string, ch chan<- MetricNames, counter int) error {
+			params := url.Values{}
+			params.Add("name", filter)
+
+			body, err := api.req(path, params.Encode())
+			if err != nil {
+				log.Error("Error getting metric names: ", err)
+				return err
+			}
+
+			dec := json.NewDecoder(bytes.NewReader(body))
+
+			metricNamesBuffer := make(MetricNames)
+			var lastKey string
+			var curName string
+			var curValues []string
+
+			for {
+				t, err := dec.Token()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if t == "metrics" || t == "name" || t == "values" {
+					lastKey = t.(string)
+					continue
+				}
+
+				if _, ok := t.(json.Delim); ok {
+					continue
+				}
+
+				if lastKey == "name" {
+					curName = t.(string)
+				} else if lastKey == "values" {
+					curValues = append(curValues, t.(string))
+				}
+
+				if !dec.More() && lastKey == "values" {
+					// finalizing metric addition
+					metricNamesBuffer[curName] = curValues
+					curValues = curValues[0:0]
+				}
+			}
+
+			ch <- metricNamesBuffer
+
+			return nil
+		}(filter, channel, i)
+	}
+
+	for {
+		mnBuffer := <-channel
+		mm := *m
+		for mn, mv := range mnBuffer {
+			mm[mn] = mv
+		}
+
+		if i--; i < 0 {
+			break
+		}
+	}
+
+	return nil
+}
+
+type MetricData struct {
+	Metric_Data struct {
+		Metrics []struct {
+			Name       string
+			Timeslices []struct {
+				Values map[string]interface{}
+			}
+		}
+	}
+}
+
+func (m *MetricData) Get(api *NewRelicAPI, appID int, names MetricNames) error {
+	path := fmt.Sprintf("/v2/%s/%s/metrics/data.json", api.service, strconv.Itoa(appID))
+
+	var nameList []string
+
+	for name, _ := range names {
+		// We urlencode the metric names as the API will return
+		// unencoded names which it cannot read
+		nameList = append(nameList, name)
+	}
+	log.Infof("Requesting %d metrics for application id %d.", len(nameList), appID)
+
+	// Because the Go client does not yet support 100-continue
+	// ( see issue #3665 ),
+	// we have to process this in chunks, to ensure the response
+	// fits within a single request.
+
+	chans := make([]chan MetricData, 0)
+
+	for i := 0; i < len(nameList); i += ChunkSize {
+
+		chans = append(chans, make(chan MetricData))
+
+		var thisList []string
+
+		if i+ChunkSize > len(nameList) {
+			thisList = nameList[i:]
+		} else {
+			thisList = nameList[i : i+ChunkSize]
+		}
+
+		go func(names []string, ch chan<- MetricData) {
+
+			var data MetricData
+
+			params := url.Values{}
+
+			for _, thisName := range names {
+				params.Add("names[]", thisName)
+			}
+
+			if len(cfg.NRValueFilters) > 0 {
+				for _, valueFilter := range cfg.NRValueFilters {
+					params.Add("values[]", valueFilter)
+				}
+			}
+
+			params.Add("raw", "true")
+			params.Add("summarize", "true")
+			params.Add("period", strconv.Itoa(api.Period))
+			params.Add("from", api.From.Format(time.RFC3339))
+			params.Add("to", api.To.Format(time.RFC3339))
+
+			body, err := api.req(path, params.Encode())
+			if err != nil {
+				log.Error("Error requesting metrics: ", err)
+				close(ch)
+				return
+			}
+
+			dec := json.NewDecoder(bytes.NewReader(body))
+			for {
+
+				page := new(MetricData)
+				if err := dec.Decode(page); err == io.EOF {
+					break
+				} else if err != nil {
+					log.Error("Error decoding metrics data: ", err)
+					close(ch)
+					return
+				}
+
+				data.Metric_Data.Metrics = append(data.Metric_Data.Metrics, page.Metric_Data.Metrics...)
+
+			}
+
+			ch <- data
+			close(ch)
+
+		}(thisList, chans[len(chans)-1])
+
+	}
+
+	allData := m.Metric_Data.Metrics
+
+	for _, ch := range chans {
+		m := <-ch
+		allData = append(allData, m.Metric_Data.Metrics...)
+	}
+	m.Metric_Data.Metrics = allData
+
+	return nil
+}
